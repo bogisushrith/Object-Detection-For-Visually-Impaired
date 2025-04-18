b19d641d63c97bd386ac325dff2ea6a4cf839f49
Sushrith <116748982+bogisushrith@users.noreply.github.com>
2025-04-19 00:45:34 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application") version "8.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+
+    // Dagger Hilt Plugin
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+}
